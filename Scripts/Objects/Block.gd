b6557fca4d72456250203ff423af12008281ce23
ben <benjaminshurts@gmail.com>
2020-09-15 22:43:28 -0600

    tools

--- Scripts/Objects/Block.gd
+++ Scripts/Objects/Block.gd
@@ -1,5 +1,28 @@
+tool
 extends Node2D
 
 export(String) var blockType:String = "Block"
+export(Texture) var blockSprite:Texture
 
- 
+
+func _change_sprite(sprite):
+	#if Engine.editor_hint:
+	pass
+	
+func _process(delta):
+	if Engine.editor_hint:
+		getallnodes(get_node())
+	pass
+
+func _loop_through_children(name):
+
+	pass
+
+func getallnodes(node):
+	for N in node.get_children():
+		if N.get_child_count() > 0:
+			print("["+N.get_name()+"]")
+			getallnodes(N)
+		else:
+			# Do something
+			print("- "+N.get_name())
