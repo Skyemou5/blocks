6b1744a6bc7bc3b1f0e76b9845d00610e3e28d24
ben <benjaminshurts@gmail.com>
2020-09-23 15:41:57 -0600

    tools

--- Scripts/Objects/Block.gd
+++ Scripts/Objects/Block.gd
@@ -2,29 +2,13 @@ tool
 extends Node2D
 
 
-export(String) var blockType:String = "Block"
-export(Texture) var blockSprite:Texture
+export(String) onready var blockType:String = "Block"
+export(Texture) onready var blockSprite:Texture
 
-
+onready var _get_spr_node = $Sprite 
 func _change_sprite(sprite):
-	#if Engine.editor_hint:
-	pass
-	
+	if Engine.editor_hint:
+		_get_spr_node.texture = sprite
+
 func _process(delta):
-	
-	#if Engine.editor_hint:
-		#getallnodes()
-	pass
-
-func _loop_through_children(name):
-
-	pass
-
-func getallnodes(node):
-	for N in node.get_children():
-		if N.get_child_count() > 0:
-			print("["+N.get_name()+"]")
-			getallnodes(N)
-		else:
-			# Do something
-			print("- "+N.get_name())
+	_change_sprite(blockSprite)
