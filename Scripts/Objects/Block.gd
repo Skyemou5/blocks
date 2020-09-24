723ac7e7d7fb1e2174866390e88a0e766465132a
ben <benjaminshurts@gmail.com>
2020-09-24 12:20:04 -0600

    change sprite from root working

--- Scripts/Objects/Block.gd
+++ Scripts/Objects/Block.gd
@@ -5,10 +5,16 @@ extends Node2D
 export(String) onready var blockType:String = "Block"
 export(Texture) onready var blockSprite:Texture
 
-onready var _get_spr_node = $Sprite 
-func _change_sprite(sprite):
+onready var _get_spr_node = $KinematicBody2D/Block1
+
+func _ready():
+	if not Engine.editor_hint:
+		_get_spr_node.texture = blockSprite
+
+func _change_sprite(spr):
 	if Engine.editor_hint:
-		_get_spr_node.texture = sprite
+		_get_spr_node.texture = spr
 
 func _process(delta):
 	_change_sprite(blockSprite)
+	
